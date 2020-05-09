c9df552a655c55356205a1b40465791b0784784a
ANM699 <kevin_kuo@126.com>
2020-05-09 16:56:07 +0800

    引入路由react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import Login from './containers/login/login'
+import Register from './containers/register/register'
+import Main from './containers/main/main'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route component={Main} />
+    </Switch>
+  </HashRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
